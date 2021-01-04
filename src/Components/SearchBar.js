fc6649eaef7b49aa45b4a6c68c9fd57831ce99e8
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-04 11:18:33 +0530

    before start working on component on 4-1-2021

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,17 +2,15 @@ import React from 'react'
 
 export default class SearchBar extends React.Component{
 
-    onTextInputChange(event)
-    {
-        console.log(event.target.value)
-    }
+    state = { search:''}
+
     render(){
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onTextInputChange}/>
+                        <input type="text" value={this.state.search} onChange={(e)=> {this.setState({search:e.target.value})}}/>
                         {/* <input type="text" onChange={this.onTextInputChange() -> this will call on readering a component so awoid it}/> */}
                     </div>
                 </form>
