6431aad08a28b65e5f8ec6a423f4a0248506b3e1
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-04 13:24:06 +0530

    After installing AXIOS and getting response from unsplash api using axios along with asyns and await - 4-1-21

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component{
 
-    onSearchSubmit(search)
+    async onSearchSubmit(search)
     {
         console.log(search,'getting value form child component');
+       const output = await axios.get('https://api.unsplash.com/search/photos',{
+            params:{
+                query:search
+            },
+            headers:{
+                Authorization: 'Client-ID PBGi7Q5g1Ar9-XfoWiPp6i9sy9Jbxp0BKQaIt82dl1w'
+            }
+        });
+
+        console.log(output.data.results);
     }
     render()
     {
