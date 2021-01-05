55b607c373048ada414484accec8c1cdc4dddd20
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-05 15:04:07 +0530

    Images grid application completed using react js - 5-1-21

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,11 +1,16 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) =>{
-    const imageList = props.images.map(({id,description,urls})=>{
-        return <img key={id} alt={description} src={urls.regular}/>
+    const imageList = props.images.map((img)=>{
+        return <ImageCard key={img.id} image={img}/>
     })
 
-    return <div>{imageList}</div>;
+    // const imageList = props.images.map(({id,description,urls})=>{
+    //     return <img key={id} alt={description} src={urls.regular}/>
+    // })
+    return <div className="image-list">{imageList}</div>;
 }
 
 export default ImageList;
